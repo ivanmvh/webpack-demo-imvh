216ddd595687fbc543dacb01dcea64226f07b34d
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-12-07 18:58:50 -0500

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import Icon from './bg-una-pareja-tango-desktop.png';
 
 function component() {
   const element = document.createElement('div');
-  
+
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello Ivan 14:52', 'Webpack'], ' ');
   element.classList.add('hello');
-    
+
   // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
-  element.appendChild(myIcon);  
+  element.appendChild(myIcon); 
   return element;
 }
-  
+
 document.body.appendChild(component());
