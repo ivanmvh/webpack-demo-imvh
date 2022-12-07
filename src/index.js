6a6b24fb4df77d1ca1755c43d2808a5ea4975002
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-12-07 14:58:58 -0500

    Configuration webpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './bg-una-pareja-tango-desktop.png';
 
- function component() {
-   const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello Ivan', 'webpack'], ' ');
-
-   return element;
- }
-
- document.body.appendChild(component());
+function component() {
+    
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello Ivan 14:52', 'Webpack'], ' ');
+    element.classList.add('hello');
+    
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+ 
+    element.appendChild(myIcon);
+    
+    return element;
+  }
+  
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
