1a4afa89638bfa3de29ee3627aedbe54112a392d
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-12-07 18:56:22 -0500

    delete node_modules and fix linters errors

--- src/index.js
+++ src/index.js
@@ -3,21 +3,18 @@ import './style.css';
 import Icon from './bg-una-pareja-tango-desktop.png';
 
 function component() {
-    
-    const element = document.createElement('div');
+  const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello Ivan 14:52', 'Webpack'], ' ');
-    element.classList.add('hello');
+  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello Ivan 14:52', 'Webpack'], ' ');
+  element.classList.add('hello');
     
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
- 
-    element.appendChild(myIcon);
-    
-    return element;
-  }
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);  
+  return element;
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
